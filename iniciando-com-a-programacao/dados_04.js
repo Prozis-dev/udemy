16f88b72706edbce1d4978759e701eacc751ce0d
Gabriel Medeiros Nóbrega <headoneshoter@gmail.com>
2025-02-04 16:10:34 -0300

    Finalizando S3

---
+++ iniciando-com-a-programacao/dados_04.js
@@ -0,0 +1,15 @@
+
+var a = 10;
+
+a = a+3;
+
+console.log(a);
+
+//Para criar uma variavel que não altere seu reseutado usa o "const="
+
+const b = 10;
+
+/* b = b+3   No caso isso daria erro, pois a variavel b é uma constante logo,
+não alterará
+*/
+console.log(b)
---
+++ iniciando-com-a-programacao/desafio_area.js
@@ -0,0 +1,8 @@
+//Pi * raio * raio
+//Algo constante coloca letra maiúscula 
+
+let raio = 10;
+const PI = 3.14;
+let areaCirc = PI*raio*raio;
+
+console.log("O valor da área é "+ areaCirc +"m²")
