acf195ec4369c1967a279237295b24f2b11aae9e
Gabriel Medeiros Nóbrega <headoneshoter@gmail.com>
2025-02-07 18:45:58 -0300

    S4 - 38 O básico Var,Let e Const

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,20 @@
+var a = 3;
+let b = 5;
+
+/*O "var" permite redeclarar a mesma variavel no mesmo código
+O "let" não permite isso
+*/
+
+var a = 43;
+b = 22;
+
+console.log(a, b);
+
+a=300;  
+b=400;
+
+console.log(a, b);
+
+const c = 50;
+
+console.log(a,b,c);
